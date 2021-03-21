c6aee8b910880ab16e20e164c5e8c49117d5065a
sirkevich <saharainternational2@gmail.com>
2021-03-21 14:56:52 +0200

    Created arrays; Wrote array;

--- src/main/java/com/classes/hillel/Main.java
+++ src/main/java/com/classes/hillel/Main.java
@@ -1,8 +1,21 @@
 package main.java.com.classes.hillel;
 
+import javax.swing.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Привет");
-        int a = 4;
+    public static void main(String[] args){
+        String[]array=new String[2];
+        int[]array2={1,2,3};
+
+        array[0] = "Hello";
+        array[1] = "World";
+
+        System.out.println(array[array.length-1]);
+
+       // for (int i = 0; i<array.length; i++) {
+         //   System.out.println(array[i]);
+        //} for (int i = 0; i<array2.length; i++) {
+         //   System.out.println(array2[i]);
+        //}
     }
 }
