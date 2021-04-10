b88f09b1a736b71418e805806c36aec337da3e46
DanVode <darksoulsdanial64@gmail.com>
2021-04-10 22:01:08 +0300

    ДЗ 5, сканер

--- src/main/java/com/classes/hillel/Main.java
+++ src/main/java/com/classes/hillel/Main.java
@@ -1,49 +1,21 @@
 package main.java.com.classes.hillel;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
 public class Main {
-    public static void main(String[] args) {
-        String[] array = new String[2];
-        int[] array2 = {1, 2, 3};
-
-        array[0] = "Hello";
-        array[1] = "Hello World";
-
-        System.out.println(array[array.length - 1]);
-
-        String[] alphabet = {"a", "b", "c", "d",
-                "e", "f", "g", "h", "i", "j", "k",
-                "l", "m", "n", "o", "p", "q", "r",
-                "s", "t", "u", "v", "w", "x", "y", "z", " "};
-
-        String encryptedWord = encrypt("Hello World",
-                alphabet, 3);
-        System.out.println(encryptedWord);
-    }
-
-    private static String encrypt(String word,
-                                String[] alphabet,
-                                int shift){
-        String encrypted = "";
-        for (int i = 0; i < word.length(); i++){
-            int index = findIndex(alphabet, word.charAt(i));
-            if (index+shift > alphabet.length-1){
-                int innerIndex = (index+shift) - alphabet.length;
-                index = innerIndex-1;
-            }
-            encrypted = encrypted.concat(alphabet[index+shift]);
-        }
-        return encrypted;
-    }
-
-    private static int findIndex(String[] alphabet,
-                                 char letter) {
-        int index = 0;
-        for (int i = 0; i<alphabet.length; i++){
-            String ch = alphabet[i];
-            if (ch.equals(String.valueOf(letter))) {
-                return i;
-            }
-        }
-        return index;
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Ведите имя: ");
+        String name = in.nextLine();
+        System.out.print("Ведите возрост: ");
+        int age = in.nextInt();
+        System.out.print("Ведите рост: ");
+        float height = in.nextFloat();
+        System.out.printf("Имя: %s  Возрост: %d  Рост: %.2f \n", name, age, height);
+        in.close();
     }
 }
