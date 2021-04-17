7da7bec831c29db30c07eeda81e87d622fed71a2
DanVode <darksoulsdanial64@gmail.com>
2021-04-17 21:41:53 +0300

    ДЗ 6, сортировка

--- src/main/java/com/classes/hillel/Main.java
+++ src/main/java/com/classes/hillel/Main.java
@@ -1,18 +1,28 @@
 package main.java.com.classes.hillel;
 
-import java.util.Scanner;
-
+import java.util.Arrays;
 
 public class Main {
-    public static void main (String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Ведите имя: ");
-        String name = in.nextLine();
-        System.out.print("Ведите возрост: ");
-        int age = in.nextInt();
-        System.out.print("Ведите рост: ");
-        float height = in.nextFloat();
-        System.out.printf("Имя: %s  Возрост: %d  Рост: %.2f \n", name, age, height);
-        in.close();
+    public static void main(String[] args) {
+        int[] array = {4, 2,6, 9,2,7};
+        boolean a = true;
+        while (a) {
+            a = false;
+            for (int i = 1; i < array.length; i++) {
+                if (array[i] < array[i - 1]) {
+                    swap(array, i, i-1);
+                    a = true;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+
+    }
+
+    private static void swap(int[] array, int b1, int b2) {
+        int tmp = array[b1];
+        array[b1] = array[b2];
+        array[b2] = tmp;
     }
 }
+
